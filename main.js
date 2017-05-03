f8a8ab4d6b10ef728d10da848fa9c786f26951f2
Birk Johansson <birk@dhis2.org>
2017-05-03 16:25:53 +0200

    Fix secret.

--- main.js
+++ main.js
@@ -16,9 +16,11 @@ client.on('message', msg => {
     const AOD = client.guilds.find('name', GUILD_NAME);
     const voices = AOD.channels.filter(chan => chan.type === 'voice');
     const lobby = AOD.channels.find(chan => chan.name === "Lobby");
-    const councilChannel = voices.find(chan => chan.name === LOOT_COUNCIL_ROLE);
+    const councilChannel = voices.find(chan => chan.name === 'Raid');
     const councilRole = AOD.roles.find('name', LOOT_COUNCIL_ROLE);
     const councilMembers = councilRole.members;
+    const msgContent = msg.content.split(' ');
+    console.log(msgContent)
     const padni = councilMembers.filter(member => member.user.username === 'Padni').first()
     if (msg.content === 'ping') {
         msg.reply('Pong!');
@@ -33,7 +35,8 @@ client.on('message', msg => {
         }));
         msg.reply("Moved Loot Council")
     }
-    else if (msg.content === 'mvB') {
+    else if (msgContent[0] === 'mvCb') {
+
         councilMembers.every(member => member.setVoiceChannel(lobby)
             .then( member => {
                 console.log("Moved " + member.user.username);
@@ -42,6 +45,39 @@ client.on('message', msg => {
                 console.log("Failed to move user " + member.user.username)
             }));
         msg.reply("Moved Loot Council")
+    }
+
+    else if (msgContent[0] === 'mvA') {
+        console.log("move all")
+        if(!msgContent[1] && !msgContent[2]) {
+            console.log("No target channel");
+            return;
+        }
+        const fromChan = voices.find(chan => chan.name === msgContent[1])
+        const targetChan = voices.find(chan => chan.name === msgContent[2]);
+        if(targetChan && fromChan) {
+            console.log(targetChan)
+            fromChan.members.every(member => {
+                console.log(member)
+                member.setVoiceChannel(targetChan).then((mem) =>
+                console.log("Moved " + mem.user.username)).catch(err =>
+                console.log("Failed to move: " + err));
+            });
+        } else {
+            console.log("Target channel " + targetChan + " not found.")
+        }
+
+    }
+    else if (msgContent[0] === 'mvB') {
+        if(!msgContent[1]) {
+            console.log("No target channel");
+            return;
+        }
+        const targetChan = voices.find(chan => chan.name === msgContent[1]);
+        if(targetChan.length > 0) {
+            targetChan.members.every(member => member.setVoiceChannel(targetChan));
+        }
+
     }
 });
 
