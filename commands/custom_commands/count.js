cf7c8aecf7da70b83bd5af97bb864a576e5a50a6
Joakim <joakimtangnes@gmail.com>
2017-05-04 00:14:51 +0200

    count command

---
+++ commands/custom_commands/count.js
@@ -0,0 +1,29 @@
+module.exports = (msg, guild, command) => {
+  const roomName = command.args[0];
+    const answerChannel = guild.channels.find(chan => chan.id === msg.channel.id);
+  if(roomName){
+      const countChannel = guild.channels.find(chan => chan.type === 'voice' && chan.name.toLowerCase() === roomName.toLowerCase());
+      if(!countChannel){
+          msg.reply(` ${roomName} is not a valid room`);
+          return;
+      }
+      const count = countChannel.members.array().length;
+      answerChannel.send(
+          `Voice channel ${countChannel.name} ` +
+          `have ${count} ` +
+          `connected client${count !== 1 ? "s" : ""}`
+      );
+  }else{
+      const countChannels = guild.channels.filter(chan => chan.type === 'voice');
+      let totalCount = 0;
+      countChannels.every(channel => {
+          totalCount += channel.members.array().length;
+          return true;
+      });
+      answerChannel.send(
+          `All voice channels ` +
+          `have ${totalCount} total ` +
+          `connected client${totalCount !== 1 ? "s" : ""}`
+      );
+  }
+};
