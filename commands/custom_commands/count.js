c29ad38513842f45bacc24cef64f33e39320c265
Birk Johansson <birk@dhis2.org>
2017-05-04 18:01:22 +0200

    Count uses current connected voicechannel by default.

--- commands/custom_commands/count.js
+++ commands/custom_commands/count.js
@@ -1,32 +1,42 @@
 module.exports = (msg, guild, command) => {
 
-  const roomName = command.args[0];
-  const answerChannel = guild.channels.find(chan => chan.id === msg.channel.id);
+    const roomName = command.args[0];
+    const answerChannel = guild.channels.find(chan => chan.id === msg.channel.id);
 
-  if(roomName){
-      const countChannel = guild.channels.find(chan => chan.type === 'voice' && chan.name.toLowerCase() === roomName.toLowerCase());
-      if(!countChannel){
-          msg.reply(` ${roomName} is not a valid room`);
-          return;
-      }
-      const count = countChannel.members.array().length;
-      answerChannel.send(
-          `Voice channel ${countChannel.name} ` +
-          `have ${count} ` +
-          `connected client${count !== 1 ? "s" : ""}`
-      );
+    if (roomName) {
+        const countChannel = guild.channels.find(chan => chan.type === 'voice' && chan.name.toLowerCase() === roomName.toLowerCase());
+        if (!countChannel) {
+            msg.reply(` ${roomName} is not a valid room`);
+            return;
+        }
+        const count = countChannel.members.array().length;
+        answerChannel.send(
+            `Voice channel ${countChannel.name} ` +
+            `has ${count} ` +
+            `connected client${count !== 1 ? "s" : ""}`
+        );
 
-  }else{
-      const countChannels = guild.channels.filter(chan => chan.type === 'voice');
-      let totalCount = 0;
-      countChannels.every(channel => {
-          totalCount += channel.members.array().length;
-          return true;
-      });
-      answerChannel.send(
-          `All voice channels ` +
-          `have ${totalCount} total ` +
-          `connected client${totalCount !== 1 ? "s" : ""}`
-      );
-  }
-};
+    } else if (msg.member && msg.member.voiceChannel) {
+        const countChannel = msg.member.voiceChannel;
+        const count = countChannel.members.array().length;
+        answerChannel.send(
+            `Voice channel ${countChannel.name} ` +
+            `has ${count} ` +
+            `connected client${count !== 1 ? "s" : ""}`
+        )
+    }
+
+    else {
+        const countChannels = guild.channels.filter(chan => chan.type === 'voice');
+        let totalCount = 0;
+        countChannels.every(channel => {
+            totalCount += channel.members.array().length;
+            return true;
+        });
+        answerChannel.send(
+            `All voice channels ` +
+            `have ${totalCount} total ` +
+            `connected client${totalCount !== 1 ? "s" : ""}`
+        );
+    }
+}
